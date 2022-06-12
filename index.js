a37ade1bc254cc4b9cd46b230fd5f765f71bc3f6
Benjamin Klarer <benjaminklarer@gmail.com>
2022-06-12 13:30:27 -0700

    commit tree 6d352f9d6f18e6773183cfeeca7c0bb711005ac4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(firstName, email) {
+    return fetch("http://localhost:3000/users", {
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name": firstName,
+            "email":email
+        })
+    })
+        .then(res => res.json())
+        .then(data => renderId(data))
+    .catch((error) => {
+        document.body.innerHTML = error;
+    })
+        
+}
+
+const renderId = data => {
+    document.body.innerHTML = data.id;
+}
